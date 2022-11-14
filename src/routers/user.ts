ca3657383a2507f5ddc890c4cb87953f5589ad43
bruch0 <lucas.bruch0@gmail.com>
2022-11-13 23:45:03 -0300

    add signup function, fix file name

---
+++ src/routers/user.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import * as userController from '../controllers/user';
+
+const router = Router();
+
+router.post('/signup', userController.signUp);
+
+export default router;
