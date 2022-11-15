2ea34303ea0fd364d858614455f407f40c48c4fa
bruch0 <lucas.bruch0@gmail.com>
2022-11-15 10:03:00 -0300

    add signin tests

--- tests/integration/user.spec.ts
+++ tests/integration/user.spec.ts
@@ -3,8 +3,10 @@ import faker from 'faker';
 
 import '../../src/setup';
 import app from '../../src/app';
+import clearDatabase from '../utils/clearDatabase';
+import createUser from '../utils/createUser';
 
-const request = supertest(app);
+const sut = supertest(app);
 
 describe('POST /signup', () => {
   const validBody = {
@@ -16,20 +18,54 @@ describe('POST /signup', () => {
   };
 
   it('Should return status 400 when sending invalid body', async () => {
-    const result = await request.post('/signup');
+    const result = await sut.post('/signup');
 
     expect(result.status).toEqual(400);
   });
 
   it('Should return status 201 when successfully creating an user', async () => {
-    const result = await request.post('/signup').send(validBody);
+    const result = await sut.post('/signup').send(validBody);
 
     expect(result.status).toEqual(201);
   });
 
   it('Should return status 403 when the same email is used again', async () => {
-    const result = await request.post('/signup').send(validBody);
+    const result = await sut.post('/signup').send(validBody);
 
     expect(result.status).toEqual(403);
+  });
+});
+
+describe('POST /signin', () => {
+  const validBody = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+
+  it('Should return status 400 when sendind invalid body', async () => {
+    const result = await sut.post('/signin');
+
+    expect(result.status).toEqual(400);
+  });
+
+  it('Should return status 404 when logging with an inexistent account', async () => {
+    await clearDatabase();
+    const result = await sut.post('/signin').send(validBody);
+
+    expect(result.status).toEqual(404);
+  });
+
+  it('Should return status 401 when sent password is wrong', async () => {
+    const user = await createUser();
+    const result = await sut.post('/signin').send({ email: user.email, password: faker.internet.password() });
+
+    expect(result.status).toEqual(401);
+  });
+
+  it('Should return status 200 and a token when successfully logging in', async () => {
+    const user = await createUser();
+    const result = await sut.post('/signin').send({ email: user.email, password: user.password });
+    console.log(result.body);
+    expect(result.status).toEqual(200);
   });
 });
