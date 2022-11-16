9ff30fc598787fefd52e53c1161ffaa717785f34
bruch0 <lucas.bruch0@gmail.com>
2022-11-16 14:37:56 -0300

    refactor

--- tests/utils/createToken.ts
+++ tests/utils/createToken.ts
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
 import createUser from './createUser';
 
-const createToken = async (): Promise<string> => {
+const createToken = async (customId?: number): Promise<string> => {
   const user = await createUser();
 
   const sessionToken = jwt.sign(
     {
       ...user,
       password: null,
+      id: customId ?? user.id,
     },
     process.env.JWT_SECRET,
     { expiresIn: 60 * 60 }
