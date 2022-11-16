94c7a1d85f5e1413e143942a6b35e66cc18fab61
bruch0 <lucas.bruch0@gmail.com>
2022-11-16 13:24:06 -0300

    fix middleware

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -4,8 +4,7 @@ import jwt from 'jsonwebtoken';
 
 const authentication = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
   const authHeader = request.headers.authorization;
-  if (authHeader == null) return response.sendStatus(401);
-
+  if (authHeader == null || !/Bearer [a-zA-Z0-9]/.test(authHeader)) return response.sendStatus(401);
   const token = authHeader.replace('Bearer ', '');
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
