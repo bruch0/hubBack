2870845d1507d68cbecc25158efaa2d39ebe132c
bruch0 <lucas.bruch0@gmail.com>
2022-11-16 07:37:36 -0300

    add crud functions on repositories

---
+++ src/repositories/company.ts
@@ -0,0 +1,38 @@
+import { Prisma, PrismaClient, Company } from '@prisma/client';
+
+const getCompanies = async (companyFindMany: Prisma.CompanyFindManyArgs): Promise<Company[]> => {
+  const prisma = new PrismaClient();
+  const response = await prisma.company.findMany(companyFindMany);
+
+  return response;
+};
+
+const getOneCompany = async (CompanyFindUnique: Prisma.CompanyFindUniqueArgs): Promise<Company | null> => {
+  const prisma = new PrismaClient();
+  const response = await prisma.company.findUnique(CompanyFindUnique);
+
+  return response;
+};
+
+const createCompany = async (companyCreate: Prisma.CompanyCreateArgs): Promise<Company> => {
+  const prisma = new PrismaClient();
+  const response = await prisma.company.create(companyCreate);
+
+  return response;
+};
+
+const updateCompany = async (companyUpdate: Prisma.CompanyUpdateArgs): Promise<Company> => {
+  const prisma = new PrismaClient();
+  const response = await prisma.company.update(companyUpdate);
+
+  return response;
+};
+
+const deleteCompany = async (companyDelete: Prisma.CompanyDeleteArgs): Promise<Company> => {
+  const prisma = new PrismaClient();
+  const response = await prisma.company.delete(companyDelete);
+
+  return response;
+};
+
+export { getCompanies, getOneCompany, createCompany, updateCompany, deleteCompany };
