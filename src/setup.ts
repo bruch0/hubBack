252c8dfeb6fe36bf0efd00dd3e90d2a61109fcbc
bruch0 <lucas.bruch0@gmail.com>
2022-11-16 15:10:11 -0300

    add fixes to database, remove aditional schema

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
 
-const envFile = '.env';
+let envFile: string | undefined;
+
+if (process.env.NODE_ENV === 'test') {
+  envFile = '.env.test';
+} else {
+  envFile = '.env';
+}
 
 dotenv.config({
   path: envFile,
